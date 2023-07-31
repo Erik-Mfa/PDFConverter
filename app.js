a86f9508be19934a44d84b258b818d136b176a21
Erik-Mfa <erikmfa190230@gmail.com>
2023-07-31 13:00:39 -0300

    PDF converter

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const app = express();
+
 const upload = require("./src/multer/multer")
-const read = require("./src/Pdf/generate")
-const pdfRouter = require("./routes/pdfRouter")
 const fs = require("fs");
-// const ejs = require("ejs");
-// const path = require("path")
+const path = require("path");
+var docxConverter = require('docx-pdf');
+const ejs = require("ejs");
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-app.use('/pdf', pdfRouter);
 
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.post("/upload", upload.single("file"), (req, res) => {
-    var file = './uploads/' + req.file.filename;
-    var document = fs.readFileSync(file, 'utf8');
-   
-    console.log(document)
-    res.send("enviado");
+app.post("/pdf", upload.single("file"), (req, res) => {
+    let outputfilepath = Date.now() + "output.pdf"
+
+    docxConverter(req.file.path, outputfilepath ,function(err,result){
+        if(err){
+            console.log(err);
+        }
+        console.log('result'+result);
+    });
+
+    res.send("OK")
 })
 
 app.listen(3000, () => {
